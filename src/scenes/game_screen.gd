c6f77e6d3f19e2340f7de4769ff348a12d9d3c97
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-01-24 20:07:43 +0100

    spawn bubbles in columns

--- src/scenes/game_screen.gd
+++ src/scenes/game_screen.gd
@@ -5,17 +5,14 @@ var bubble_scene = preload("res://src/objects/Bubble.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var screen_size = get_viewport().get_visible_rect().size
-	
-	var min_x = 0
-	var max_x = screen_size.x
-	var min_y = 0
-	var max_y = screen_size.y
 
-	for i in range(10):
-		var bubble = bubble_scene.instantiate()
-		var random_position = Vector2(randf_range(min_x, max_x), randf_range(min_y, max_y))
-		bubble.position = random_position
-		add_child(bubble)
+	var step_x = screen_size.x / 3;
+	var step_y = screen_size.y / 3;
+	for i in range(3):
+		for j in range(3):
+			var bubble = bubble_scene.instantiate()
+			bubble.position = Vector2(step_x * i + step_x / 2, step_y * j + step_y / 2)
+			add_child(bubble)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
