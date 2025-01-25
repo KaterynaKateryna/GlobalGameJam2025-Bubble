cdf8e7407aed0e09f312fcff28dac5281969e8c3
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-01-25 10:34:28 +0100

    change screen size, change bubble spawning

--- src/scenes/game_screen.gd
+++ src/scenes/game_screen.gd
@@ -5,15 +5,20 @@ var bubble_scene = preload("res://src/objects/Bubble.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var screen_size = get_viewport().get_visible_rect().size
-
-	var step_x = screen_size.x / 3;
-	var step_y = screen_size.y / 3;
+	var middle_x = screen_size.x / 2;
+	var step_x = 350;
+	var step_y = 350;
 	for i in range(3):
-		for j in range(3):
-			var bubble = bubble_scene.instantiate()
-			bubble.position = Vector2(step_x * i + step_x / 2, step_y * j + step_y / 2)
-			add_child(bubble)
-
+		var y = screen_size.y - i * step_y - 350;
+		_create_bubble(middle_x, y)
+		_create_bubble(middle_x - step_x, y)
+		_create_bubble(middle_x + step_x, y)
+			
+			
+func _create_bubble(x: float, y: float):
+	var bubble = bubble_scene.instantiate()
+	bubble.position = Vector2(x, y)
+	add_child(bubble)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
