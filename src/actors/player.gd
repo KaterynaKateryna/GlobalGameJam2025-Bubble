e5639be1f493c4fd85e480d07f5953dd59d3ca88
KaterynaKateryna <tok.ar.tok@gmail.com>
2025-01-26 11:38:32 +0100

    pop bubbles, randomize bubble movement, improve jumping

--- src/actors/player.gd
+++ src/actors/player.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY_Y = -800.0
-const JUMP_VELOCITY_X = -150.0
+const JUMP_VELOCITY_Y = -900.0
+const JUMP_VELOCITY_X = -180.0
 
 var is_on_duck = false
+var is_on_bubble = false
+var bubble = null
 
 func _ready():
 	var viewport = get_viewport()
@@ -18,6 +20,10 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		velocity.x = 0
 		
+	if is_on_bubble and bubble:
+		velocity.x = bubble.velocity.x
+		velocity.y = bubble.velocity.y
+		
 	if is_on_duck:
 		var duck = get_tree().root.get_node("GameScreen/Duck")
 		velocity.x = duck.velocity.x
@@ -27,18 +33,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_bubble):
+		print("jump")
 		velocity.y = JUMP_VELOCITY_Y
 		velocity.x = JUMP_VELOCITY_X
 
 		var mousePosition = get_global_mouse_position()
 		var direction = mousePosition.x - position.x
+		print(direction)
+		if (direction > -75 and direction < 75):
+			velocity.x = 0
+		
 		if (direction > 0):
 			velocity.x = velocity.x * -1.0
 
 	move_and_slide()
 
 func _on_bubble_detector_body_entered(body: Node2D) -> void:
+	if velocity.y < 0:
+		return # going up
+	is_on_bubble = true
+	bubble = body
+	
 	var modifier = body.get_modifier()
 	print("bubble jumped body ", modifier)
 	var operator = modifier.substr(0,1)
@@ -52,7 +68,13 @@ func _on_bubble_detector_body_entered(body: Node2D) -> void:
 		GameData.score *= operand_int
 	if operator == "%":
 		GameData.score /= operand_int
-
+		
+func _on_bubble_detector_body_exited(body: Node2D) -> void:
+	if is_on_bubble and bubble:
+		is_on_bubble = false
+		bubble.queue_free()	
+		bubble = null
+		print("bubble left")
 
 func _on_duck_detector_body_entered(body: Node2D) -> void:
 	is_on_duck = true
--- src/objects/bubble.gd
+++ src/objects/bubble.gd
@@ -1,19 +1,59 @@
 extends StaticBody2D
 
 @onready var modifier_label = get_node("ModifierLabel")
+@onready var sprite = get_node("Sprite2D")
+
+var previous_position: Vector2
+var velocity: Vector2 = Vector2.ZERO
 
 var modifier = null
+
+var bound_value = 50
+
+var bounds_left
+var bounds_right
+var bounds_top
+var bounds_bottom
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if modifier:
 		modifier_label.text = modifier
-	pass # Replace with function body.
+		
+	previous_position = position
+		
+	var center_x = position.x
+	var center_y = position.y
+	
+	bounds_left = center_x - bound_value
+	bounds_right = center_x + bound_value
+	bounds_top = center_y - bound_value
+	bounds_bottom = center_y + bound_value
+	
+	_move_to_random_position()
+	
+func _move_to_random_position():
+	var random_x = randf_range(bounds_left, bounds_right)
+	var random_y = randf_range(bounds_top, bounds_bottom)
+	var target_position = Vector2(random_x, random_y)
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", target_position, 3.0)
+	
+	# Call this again once the movement is complete
+	tween.finished.connect(_on_tween_finished)
 
+func _on_tween_finished():
+	_move_to_random_position()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	# Calculate velocity by comparing the position change
+	var delta_position = position - previous_position
+	velocity = delta_position / delta  # Velocity = distance / time (frame time)
+
+	# Update previous_position for next frame
+	previous_position = position
 	
 func get_modifier():
 	if modifier:
